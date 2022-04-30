8108c0e32909639d680ead3f652e14cfac69ec82
idrisdavarci <idrisdavarci@gmail.com>
2022-05-01 00:05:10 +0300

    Initial Commit

---
+++ Getir/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Getir
+//
+//  Created by İdris Davarcı on 1.05.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
